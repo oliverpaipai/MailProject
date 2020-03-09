c40354396291f0fd39b05b0fc7d673b149238a01
oliver <15809205442@163.com>
2020-03-09 19:11:20 +0800

    commit first

---
+++ mail-service/mail-email-service/src/test/java/com/qf/mailemailservice/MailEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.mailemailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailEmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
