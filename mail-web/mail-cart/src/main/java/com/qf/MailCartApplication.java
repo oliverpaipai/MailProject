7b738ba812f595f79ccb485b7e48a1a65bed0c94
oliver <15809205442@163.com>
2020-03-11 20:57:31 +0800

    提交修改1

--- mail-web/mail-cart/src/main/java/com/qf/MailCartApplication.java
+++ mail-web/mail-cart/src/main/java/com/qf/MailCartApplication.java
@@ -1,13 +1,16 @@
 package com.qf;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDubbo
 public class MailCartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MailCartApplication.class, args);
     }
-
 }
